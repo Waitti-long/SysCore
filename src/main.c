23e2bff29af078266fe0508b6ed79da266e35475
waitti <waitti@foxmail.com>
2021-05-23 01:06:08 +0800

    1

--- src/main.c
+++ src/main.c
@@ -44,8 +44,8 @@ void init_thread() {
     lty(register_read_satp());
 
     tree_all();
-    printf("files_count = %d\n", files_count);
-    if(files_count == 36){
+    printf("files_count")
+    if(files_count > 36){
         //    create_process("/yield");
         create_process("/write");
         create_process("/uname");
